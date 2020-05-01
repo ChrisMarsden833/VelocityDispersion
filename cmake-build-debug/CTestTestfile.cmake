50fcb67a21bb2980d87b35706ddefed1ea22dd5d
Chris Marsden <chrisjmarsden@me.com>
2020-05-01 14:30:24 +0100

    removed cmake-build folders

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/christophermarsden/Documents/Astronomy/VelocityDispersion
-# Build directory: /Users/christophermarsden/Documents/Astronomy/VelocityDispersion/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(VelocityDispersionTests "/Users/christophermarsden/Documents/Astronomy/VelocityDispersion/cmake-build-debug/VelocityDispersionTests")
-set_tests_properties(VelocityDispersionTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/christophermarsden/Documents/Astronomy/VelocityDispersion/CMakeLists.txt;51;add_test;/Users/christophermarsden/Documents/Astronomy/VelocityDispersion/CMakeLists.txt;0;")
--- cmake-build-debug/Testing/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/Documents/VelocityDispersion/Testing
-# Build directory: /home/chris/Documents/VelocityDispersion/cmake-build-debug/Testing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(VelocityDispersionTests "VelocityDispersionTests")
-set_tests_properties(VelocityDispersionTests PROPERTIES  _BACKTRACE_TRIPLES "/home/chris/Documents/VelocityDispersion/Testing/CMakeLists.txt;39;add_test;/home/chris/Documents/VelocityDispersion/Testing/CMakeLists.txt;0;")
